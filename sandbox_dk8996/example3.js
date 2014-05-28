43a139d64427777d53c01ea02c0e7ce8bc1cc38a
gurlinthewurld <gurlinthewurld@gmail.com>
2014-05-28 09:59:08 -0400

    working to add month view and to understand the given functions

--- sandbox_dk8996/example3.js
+++ sandbox_dk8996/example3.js
@@ -24,9 +24,9 @@ var taskNames = [
 	"Program 6"
 ];
 tasks.sort(function(a, b) {
-    return a.endDate - b.endDate;
+    return a.endDate - b.endDate;  //?
 });
-var maxDate = tasks[tasks.length - 1].endDate;
+var maxDate = tasks[tasks.length - 1].endDate; // if there are 4 tasks, 0, 1, 2, 3 - the length is 4; the maxDate is task[2] ? 
 tasks.sort(function(a, b) {
     return a.startDate - b.startDate;
 });
@@ -78,7 +78,26 @@ function changeTimeDomain(timeDomainString) {
 	break;
     default:
 	format = "%H:%M"
-
+	
+    case "1month":
+	format = "%m %a";
+	gantt.timeDomain([ d3.time.day.offset(getEndDate(), -30), getEndDate() ]); // I need a formula for calculating if the month has 30 or 31 days 
+	 // d3.time.month
+// 
+// Months (e.g., February 1, 2012 at 12:00 AM). Ranges between 28 and 31 days.
+	// var format = d3.time.format.multi([
+//   [".%L", function(d) { return d.getMilliseconds(); }],
+//   [":%S", function(d) { return d.getSeconds(); }],
+//   ["%I:%M", function(d) { return d.getMinutes(); }],
+//   ["%I %p", function(d) { return d.getHours(); }],
+//   ["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
+//   ["%b %d", function(d) { return d.getDate() != 1; }],
+//   ["%B", function(d) { return d.getMonth(); }],
+//   ["%Y", function() { return true; }]
+// ]);
+	break;
+    default:
+	format = "%H:%M"
     }
     gantt.tickFormat(format);
     gantt.redraw(tasks);
@@ -112,7 +131,7 @@ function addTask() {
 };
 
 function removeTask() {
-    tasks.pop();
+    tasks.pop(); //removes the last task that was added
     changeTimeDomain(timeDomainString);
     gantt.redraw(tasks);
 };
