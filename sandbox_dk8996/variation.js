c00220c53f64675fee7090b65e4bd64aa5b59c12
gurlinthewurld <gurlinthewurld@gmail.com>
2014-06-04 06:56:43 -0400

    update gantt javascript, task bands data and styling

--- sandbox_dk8996/variation.js
+++ sandbox_dk8996/variation.js
@@ -1,6 +1,8 @@
   var w = 800;
   var h = 400;
 
+  w = window.innerWidth - 20;
+  h = window.innerHeight - 20;
 
   var svg = d3.selectAll(".svg")
   //.selectAll("svg")
@@ -14,25 +16,28 @@
   {
     "status": "Partner Reports",
     "taskName": "Program 1",
-    "startDate": "2013-1-28", //year/month/day
-    "endDate": "2013-2-1",
-    
+    //"startDate": "2013-1-28", //year/month/day
+    //"endDate": "2013-2-1"
+    "startDate": "Dec 14 08:40:11 EST 2014",
+    "endDate": "Dec 18 08:46:35 EST 2014"
 },
         {
-    "status": "Donor Reports",
+    "status": "Donor Report",
     "taskName": "Program 1",
-    "startDate": "2013-1-28", //year/month/day
-    "endDate": "2013-2-1",
-   
+    //"startDate": "2013-1-28", //year/month/day
+    //"endDate": "2013-2-1"
+    "startDate": "Dec 14 08:40:11 EST 2014",
+    "endDate": "Dec 18 08:46:35 EST 2014"
 },
 
 
 {
     "status": "Market Assessments",
     "taskName": "Program 4",
-    "startDate": "2013-1-28",
-    "endDate": "2013-2-6",
-    
+    //"startDate": "2013-1-28",
+    //"endDate": "2013-2-6"
+    "startDate": "Dec 11 23:12:24 EST 2014",
+    "endDate": "Dec 15 00:26:13 EST 2014"
 }
 
 
@@ -69,23 +74,25 @@ var taskNames = [
 	
 ];
 
-var dateFormat = d3.time.format("%Y-%m-%d");
+//var dateFormat = d3.time.format("%Y-%m-%d");
+var dateFormat = d3.time.format("%b %d %H:%M:%S EST %Y");
 
 var timeScale = d3.time.scale()
         .domain([d3.min(taskArray, function(d) {return dateFormat.parse(d.startDate);}),
                  d3.max(taskArray, function(d) {return dateFormat.parse(d.endDate);})])
         .range([0,w]);
 
+
 var categories = new Array();
 
 for (var i = 0; i < taskArray.length; i++){
-    categories.push(taskArray[i].type);
+    categories.push(taskArray[i].taskName);
 }
 
 var catsUnfiltered = categories; //for vert labels
 
 categories = checkUnique(categories);
-
+//categories = taskNames;
 
 makeGant(taskArray, w, h);
 
@@ -134,14 +141,15 @@ var bigRects = svg.append("g")
    })
    .attr("height", theGap)
    .attr("stroke", "none")
+   .attr("opacity", 0.1)
    .attr("fill", function(d){
     for (var i = 0; i < categories.length; i++){
-        if (d.type == categories[i]){
+        if (d.taskName == categories[i]){
           return d3.rgb(theColorScale(i));
         }
     }
-   })
-   .attr("opacity", 0.1);
+   });
+   
 
 
      var rectangles = svg.append('g')
@@ -164,14 +172,33 @@ var bigRects = svg.append("g")
              })
              .attr("height", theBarHeight)
              .attr("stroke", "none")
+             .attr("class", function(d){
+                        return taskStatus[d.status];
+                   });
+             /*
              .attr("fill", function(d){
               for (var i = 0; i < categories.length; i++){
-                  if (d.type == categories[i]){
+                  if (d.taskName == categories[i]){
                     return d3.rgb(theColorScale(i));
                   }
               }
-             })
-   
+             });
+             */
+/*
+         var rectText = rectangles.append("text")
+               .text(function(d){
+                return d.taskName;
+               })
+               .attr("x", function(d){
+                return (timeScale(dateFormat.parse(d.endDate))-timeScale(dateFormat.parse(d.startDate)))/2 + timeScale(dateFormat.parse(d.startDate)) + theSidePad;
+                })
+               .attr("y", function(d, i){
+                  return i*theGap + 14+ theTopPad;
+              })
+               .attr("font-size", 11)
+               .attr("text-anchor", "middle")
+               .attr("text-height", theBarHeight)
+               .attr("fill", "#fff");   
 
 
 
@@ -180,14 +207,12 @@ rectText.on('mouseover', function(e) {
                var tag = "";
 
          if (d3.select(this).data()[0].details != undefined){
-          tag = "Task: " + d3.select(this).data()[0].task + "<br/>" + 
-                "Type: " + d3.select(this).data()[0].type + "<br/>" + 
+          tag = "Task: " + d3.select(this).data()[0].taskName + "<br/>" + 
                 "Starts: " + d3.select(this).data()[0].startDate + "<br/>" + 
                 "Ends: " + d3.select(this).data()[0].endDate + "<br/>" + 
                 "Details: " + d3.select(this).data()[0].details;
          } else {
-          tag = "Task: " + d3.select(this).data()[0].task + "<br/>" + 
-                "Type: " + d3.select(this).data()[0].type + "<br/>" + 
+          tag = "Task: " + d3.select(this).data()[0].taskName + "<br/>" + 
                 "Starts: " + d3.select(this).data()[0].startDate + "<br/>" + 
                 "Ends: " + d3.select(this).data()[0].endDate;
          }
@@ -204,21 +229,19 @@ rectText.on('mouseover', function(e) {
          var output = document.getElementById("tag");
          output.style.display = "none";
              });
-
+*/
 
 innerRects.on('mouseover', function(e) {
  //console.log(this);
          var tag = "";
 
          if (d3.select(this).data()[0].details != undefined){
-          tag = "Task: " + d3.select(this).data()[0].task + "<br/>" + 
-                "Type: " + d3.select(this).data()[0].type + "<br/>" + 
+          tag = "Status: " + d3.select(this).data()[0].status + "<br/>" + 
                 "Starts: " + d3.select(this).data()[0].startDate + "<br/>" + 
                 "Ends: " + d3.select(this).data()[0].endDate + "<br/>" + 
                 "Details: " + d3.select(this).data()[0].details;
          } else {
-          tag = "Task: " + d3.select(this).data()[0].task + "<br/>" + 
-                "Type: " + d3.select(this).data()[0].type + "<br/>" + 
+          tag = "Status: " + d3.select(this).data()[0].status + "<br/>" + 
                 "Starts: " + d3.select(this).data()[0].startDate + "<br/>" + 
                 "Ends: " + d3.select(this).data()[0].endDate;
          }
@@ -244,23 +267,25 @@ innerRects.on('mouseover', function(e) {
 
 function makeGrid(theSidePad, theTopPad, w, h){
 
-var xAxis = d3.svg.axis()
-    .scale(timeScale)
-    .orient('bottom')
-    .ticks(d3.time.days, 1)
-    .tickSize(-h+theTopPad+20, 0, 0)
-    .tickFormat(d3.time.format('%d %b'));
+    var xAxis = d3.svg.axis()
+        .scale(timeScale)
+        .orient('bottom')
+        .ticks(d3.time.days, 1)
+        .tickSize(-h+theTopPad+20, 0, 0)
+        .tickFormat(d3.time.format('%d %b'));
 
-var grid = svg.append('g')
-    .attr('class', 'grid')
-    .attr('transform', 'translate(' +theSidePad + ', ' + (h) + ')')
-    .call(xAxis)
-    .selectAll("text")  
-            .style("text-anchor", "middle")
-            .attr("fill", "#000")
-            .attr("stroke", "none")
-            .attr("font-size", 12)
-            .attr("dy", "1em");
+    var grid = svg.append('g')
+        .attr('class', 'grid')
+        .attr('transform', 'translate(' +theSidePad + ', ' + (h) + ')')
+        .call(xAxis)
+        .selectAll("text")  
+                .style("text-anchor", "middle")
+                .attr("fill", "#000")
+                .attr("stroke", "none")
+                .attr("font-size", 12)
+                .attr("dy", "1em");
+
+                
 }
 
 function vertLabels(theGap, theTopPad, theSidePad, theBarHeight, theColorScale){
